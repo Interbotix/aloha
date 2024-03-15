3a6ec98109a75141795dd9736034e770599da18d
Luke Schmitt <luke@trossenrobotics.com>
2024-03-15 16:24:59 +0000

    Use ament_python

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
-from distutils.core import setup
+from glob import glob
+import os
 
-from catkin_pkg.python_setup import generate_distutils_setup
-
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['aloha'],
-    package_dir={'': 'src'},
+from setuptools import (
+    find_packages,
+    setup,
 )
 
+package_name = 'aloha'
+
 setup(
-    long_description=open('README.md').read(),
-    **setup_args
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude='test'),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch/*.launch.py'))),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author_email='tonyzhao@stanford.edu',
+    author='Tony Zhao',
+    maintainer='Tony Zhao',
+    maintainer_email='tonyzhao@stanford.edu',
+    description='ALOHA: A Low-cost Open-source Hardware System for Bimanual Teleoperation',
+    license='BSD',
+    tests_require=['pytest'],
+    # entry_points={
+    #     'console_scripts': [
+    #         # 'armtag_tuner_gui = aloha.armtag_tuner_gui:main',
+    #         # 'picture_snapper = aloha.picture_snapper:main',
+    #         # 'pointcloud_tuner_gui = aloha.pointcloud_tuner_gui:main',
+    #     ],
+    # },
 )
