9db9308b33c819addc495391f5d568ebe46fdfd1
Luke Schmitt <luke@trossenrobotics.com>
2024-05-13 17:34:39 +0000

    Remove missing install from setup

--- setup.py
+++ setup.py
@@ -14,10 +14,9 @@ setup(
     packages=find_packages(exclude='test'),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch/*.launch.py'))),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
