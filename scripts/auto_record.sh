603b800687d9d29a1a8c128702e1d59eec8dff11
Luke Schmitt <luke@trossenrobotics.com>
2024-05-13 19:11:35 +0000

    Update auto_record script

--- scripts/auto_record.sh
+++ scripts/auto_record.sh
@@ -1,28 +1,41 @@
-eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+ROS_DISTRO=humble
 
-source $HOME/miniconda3/etc/profile.d/conda.sh || exit 1
+VENV_ACTIVATE_PATH=$HOME/interbotix_ws/venv/bin/activate
+ROS_SETUP_PATH=/opt/ros/$ROS_DISTRO/setup.bash
+WORKSPACE_SETUP_PATH=$HOME/interbotix_ws/install/setup.bash
+RECORD_EPISODES="$HOME/interbotix_ws/src/aloha/scripts/record_episodes.py"
 
-conda deactivate || exit 1
+source $VENV_ACTIVATE_PATH || exit 1
+source $ROS_SETUP_PATH || exit 1
+source $WORKSPACE_SETUP_PATH || exit 1
 
-conda activate aloha || exit 1
-source /opt/ros/$ROS_DISTRO/setup.bash || exit 1
-source $HOME/interbotix_ws1/devel/setup.bash || exit 1 ## TODO
+print_usage() {
+  echo "USAGE:"
+  echo "auto_record.sh task num_episodes"
 
-which pip
-pip list | grep dm || exit 1
+}
+
+nargs="$#"
+
+if [ $nargs -lt 2 ]; then
+  echo "Passed incorrect number of arguments"
+  print_usage
+  exit 1
+fi
+
 
 if [ "$2" -lt 0 ]; then
   echo "# of episodes not valid"
-  exit
+  exit 1
 fi
 
 echo "Task: $1"
 for (( i=0; i<$2; i++ ))
 do
   echo "Starting episode $i"
-  rosrun aloha record_episodes.py --task "$1"
+  python3 "$RECORD_EPISODES" --task "$1"
   if [ $? -ne 0 ]; then
     echo "Failed to execute command. Returning"
-    exit
+    exit 1
   fi
 done
